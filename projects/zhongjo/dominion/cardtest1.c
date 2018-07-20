66aebb224d5bf9a81404a428e0588fb28a4f364f
johnnyczhong <johnnyczhong@gmail.com>
2018-07-19 21:54:53 -0700

    corrected discard to played card

--- projects/zhongjo/dominion/cardtest1.c
+++ projects/zhongjo/dominion/cardtest1.c
@@ -34,7 +34,7 @@ int main() {
 	struct gameState G, testG;
 	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
 			sea_hag, tribute, smithy, council_room};
-    int testCard = village;
+    int testCard = smithy;
 
 	// initialize a game state and player cards
 	initializeGame(numPlayers, k, seed, &G);
@@ -47,8 +47,8 @@ int main() {
 
 	printf("----------------- Testing buyCard: %s ----------------\n", TESTCARD);
 
-	// ----------- TEST 1: player can buy --------------
-    // check initial conditions (handCount == 5, actions == 1, smithy in discard = 0)
+    // check initial conditions 
+    // handCount == 5, actions == 1, smithy in discard = 0
     assert(testG.handCount[thisPlayer] == 5);
     assert(testG.numActions == 1);
 
@@ -59,7 +59,6 @@ int main() {
     assert(numSmithy == 0);
 
     // check final conditions (handCount == 7, actions == 0, smithy in discard == 1)
-	// ----------- TEST 1: player can buy --------------
 	printf("TEST 1: Player has played the card successfully\n");
     result = playCard(0, 0, 0, 0, &testG);
     if (result == 0) { printf("Test Passed: "); }
@@ -67,25 +66,33 @@ int main() {
 	printf("playCard result = %d, expected = %d\n", result, 0);
 
 	printf("TEST 2: Player has 2 more cards after playing\n");
-    if (testG.handCount[thisPlayer] == 7) { printf("Test Passed: "); }
+    if (testG.handCount[thisPlayer] == G.handCount[thisPlayer] + 2) { printf("Test Passed: "); }
     else { printf("Test Failed: "); }
-	printf("handCount result = %d, expected = %d\n", testG.handCount[thisPlayer], 7);
+	printf("handCount result = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + 2);
+
+	printf("TEST 3: Player drew 3 cards from deck\n");
+    if (testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - 3) { printf("Test Passed: "); }
+    else { printf("Test Failed: "); }
+	printf("deckCount result = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - 3);
 
     // check that the card is in the discard pile
     // number of this card before + 1
-    int smithyDiscardCount = 0;
-    for (i = 0; i < testG.discardCount[thisPlayer]; i++)
+	printf("TEST 4: Player has smithy in playedCard pile\n");
+    int playedSmithyCount = 0;
+    for (i = 0; i < testG.playedCardCount; i++)
     {
-        if (testG.discard[thisPlayer][i] == testCard) { smithyDiscardCount++; };
+        if (testG.playedCards[i] == testCard) { playedSmithyCount++; };
     }
-    assert(smithyDiscardCount == numSmithy + 1);
-	printf("discard count = %d, expected = %d\n", smithyDiscardCount, numSmithy + 1);
+    if (playedSmithyCount == numSmithy + 1) { printf("Test Passed: "); }
+    else { printf("Test Failed: "); }
+	printf("played count = %d, expected = %d\n", playedSmithyCount, numSmithy + 1);
 
     // check that the number of actions is 0
+	printf("TEST 5: Player has no remaining actions\n");
     assert(testG.numActions == 0);
 	printf("numActions count = %d, expected = %d\n", testG.numActions, 0);
 
-	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", "smithy");
+	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
 
 	return 0;
 }
