dca20951eed35eb4482aa1cddede5871bbf7268e
johnnyczhong <johnnyczhong@gmail.com>
2018-07-22 15:58:40 -0700

    added state change tests to cardtest1

--- projects/zhongjo/dominion/cardtest1.c
+++ projects/zhongjo/dominion/cardtest1.c
@@ -6,13 +6,6 @@
 //    2. decreases the number of actions remaining by 1
 //    3. 1 more smithy card is now in discard
 
-/*
- * Include the following lines in your makefile:
- *
- * cardtest4: cardtest4.c dominion.o rngs.o
- *      gcc -o cardtest1 -g  cardtest4.c dominion.o rngs.o $(CFLAGS)
- */
-
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -91,6 +84,50 @@ int main() {
 	printf("TEST 5: Player has no remaining actions\n");
     assert(testG.numActions == 0);
 	printf("numActions count = %d, expected = %d\n", testG.numActions, 0);
+
+	printf("TEST 6: No state changes for other players\n");
+    // ensure hand, deck, discard counts remain the same for all other players
+    for (i = 1; i < numPlayers; i++)
+    {
+        if (testG.deckCount[i] == G.deckCount[i]) { printf("Test Passed: "); }
+        else { printf("Test Failed: "); }
+        printf("deck count = %d, expected = %d\n", testG.deckCount[i], G.deckCount[i]);
+        
+        if (testG.handCount[i] == G.handCount[i]) { printf("Test Passed: "); }
+        else { printf("Test Failed: "); }
+        printf("hand count = %d, expected = %d\n", testG.handCount[i], G.handCount[i]);
+        
+        if (testG.discardCount[i] == G.discardCount[i]) { printf("Test Passed: "); }
+        else { printf("Test Failed: "); }
+        printf("discard count = %d, expected = %d\n", testG.discardCount[i], G.discardCount[i]);
+    }
+
+    printf("TEST 7: No changes to Kingdom Supply Card Piles\n");
+    int numKingdomCards = sizeof(k)/sizeof(k[0]);
+    for (i = 0; i < numKingdomCards; i++)
+    {
+        if (supplyCount(k[i], &testG) == supplyCount(k[i], &G)) { printf("Test Passed: "); }
+        else { printf("Test Failed: "); }
+        printf("kingdom count = %d, expected = %d\n", supplyCount(k[i], &testG), supplyCount(k[i], &G));
+    }
+
+    printf("TEST 8: No Changes to Victory Supply Card Piles\n");
+    int v[3] = {estate, duchy, province};
+    for (i = 0; i < 3; i++)
+    {
+        if (supplyCount(v[i], &testG) == supplyCount(v[i], &G)) { printf("Test Passed: "); }
+        else { printf("Test Failed: "); }
+        printf("victory count = %d, expected = %d\n", supplyCount(v[i], &testG), supplyCount(v[i], &G));
+    }
+
+    printf("TEST 9: No Changes to Treasure Supply Card Piles\n");
+    int t[3] = {copper, silver, gold};
+    for (i = 0; i < 3; i++)
+    {
+        if (supplyCount(t[i], &testG) == supplyCount(t[i], &G)) { printf("Test Passed: "); }
+        else { printf("Test Failed: "); }
+        printf("treasure count = %d, expected = %d\n", supplyCount(t[i], &testG), supplyCount(t[i], &G));
+    }
 
 	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
 
